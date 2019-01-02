e22ea7d090542b831b849b16f8cfbe55cc918163
tjhillard <tjhillard@gmail.com>
2019-01-02 14:47:34 -0800

    finish webpack hmr integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TodosModule } from './modules/todos/todos.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { UserEntity } from 'src/modules/users/user.entity';
+import { TodoEntity } from './modules/todos/todo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TodosModule, UsersModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: true,
+      synchronize: true,
+      entities: [
+        UserEntity,
+        TodoEntity,
+      ],
+    }),
+    TodosModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
