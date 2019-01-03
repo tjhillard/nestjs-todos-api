48a7e1eb72ed4b3a7cc49d3830aaa23298564a24
tjhillard <tjhillard@gmail.com>
2019-01-02 17:11:59 -0800

    comments and small cleanup

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,20 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // App config
   app.setGlobalPrefix('/api/v1');
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
 
+  // Swagger/OpenAPI config
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('/api/v1/docs', app, document);
 
+  // Start express server
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}  🚀 👌`, 'bootstrap()');
 
+  // Webpack HMR
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
